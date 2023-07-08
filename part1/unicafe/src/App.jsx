8f745ed611ce0db9739a14bd7ccf9f091c535a3f
Ali Alhussein <a.alhussein33@gmail.com>
2023-07-08 22:44:14 +0300

    1.8: unicafe step3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,13 +4,24 @@ export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const all = good + neutral + bad;
   return (
     <>
       <h1>Give Feedback</h1>
       <Button text="Good" handleClick={() => setGood((prev) => ++prev)} />
       <Button text="Neutral" handleClick={() => setNeutral((prev) => ++prev)} />
       <Button text="Bad" handleClick={() => setBad((prev) => ++prev)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+  );
+}
+function Button({ text, handleClick }) {
+  return <button onClick={handleClick}>{text}</button>;
+}
+
+function Statistics({ good, neutral, bad }) {
+  const all = good + neutral + bad;
+  return (
+    <>
       <h1>Statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
@@ -20,7 +31,4 @@ export default function App() {
       <p>Positive: {all > 0 && (good / all) * 100}</p>
     </>
   );
-}
-function Button({ text, handleClick }) {
-  return <button onClick={handleClick}>{text}</button>;
 }
