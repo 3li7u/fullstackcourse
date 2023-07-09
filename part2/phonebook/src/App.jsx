189585d8c39d334a8cc20206e04243f9f72a2a64
Ali Alhussein <a.alhussein33@gmail.com>
2023-07-09 23:34:01 +0300

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 
 export default function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
 
   const handleNameChange = (event) => setNewName(event.target.value);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setPersons((prev) => [...prev, { name: newName }]);
-    setNewName("");
+    persons.find((person) => person.name === newName)
+      ? alert(`${newName} is already exist!`)
+      : (setPersons((prev) => [...prev, { name: newName }]), setNewName(""));
   };
   return (
     <>
@@ -25,9 +26,11 @@ export default function App() {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(({ name }) => (
-          <li key={name}>{name}</li>
-        ))}
+        {persons.length > 0 ? (
+          persons.map(({ name }) => <li key={name}>{name}</li>)
+        ) : (
+          <li>There is no numbers yet! Add one</li>
+        )}
       </ul>
     </>
   );
