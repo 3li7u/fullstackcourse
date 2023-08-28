0e661a9d6c9d411552a5da9451e74faa6a087bd5
Ali Alhussein <a.alhussein33@gmail.com>
2023-08-28 15:14:08 +0300

    part3/notes

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,6 +1,10 @@
-const http = require("http");
+const express = require("express");
+const crypto = require("crypto");
 
-const notes = [
+const app = express();
+const PORT = 3000;
+
+let notes = [
   {
     id: 1,
     content: "HTML is easy",
@@ -18,15 +22,81 @@ const notes = [
   },
 ];
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "http://localhost:5173/",
-  });
-  res.end(JSON.stringify(notes));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-const PORT = 3000;
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
 
-app.listen(PORT);
-console.log(`Server is running on: http://localhost:${PORT}`);
+app.get("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  const note = notes.find((note) => note.id == id);
+  if (note)
+    res.json({
+      status: true,
+      code: 200,
+      message: `The requested note: ${id}, has been fetched successfully`,
+      data: note,
+    });
+  else
+    res.status(404).json({
+      status: false,
+      code: 404,
+      message: `The requested note: ${id}, hasn't been found`,
+      data: note,
+    });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  const note = notes.find((note) => note.id == id);
+  if (note) {
+    notes = notes.filter((note) => note.id != id);
+    res.json({
+      status: true,
+      code: 204,
+      message: `The requested note: ${id}, has been deleted successfully`,
+      data: note,
+    });
+  } else {
+    res.status(404).json({
+      status: false,
+      code: 404,
+      message: `The requested note: ${id}, hasn't been found`,
+      data: note,
+    });
+  }
+});
+
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  if (note?.content) {
+    const noteData = {
+      content: note.content,
+      important: note.important || false,
+      id: crypto.randomUUID(),
+    };
+    notes.push(noteData);
+    res.json({
+      status: true,
+      code: 200,
+      message: "New note has been added successfully",
+      data: noteData,
+    });
+  } else {
+    res.status(400).json({
+      status: false,
+      code: 400,
+      message: `Missing Content`,
+      data: note,
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on: http://localhost:${PORT}`);
+});
