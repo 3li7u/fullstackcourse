cdc38cf648c4c44a2abe2e4315cb1dea6beaa31f
Ali Alhussein <a.alhussein33@gmail.com>
2023-09-28 02:30:47 +0300

    part3/phonebook fix /info end point

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -1,35 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-const crypto = require("crypto");
 const morgan = require("morgan");
 const cors = require("cors");
 const { Person } = require("./models");
 const { PORT, DBConnect } = require("./config");
 const mongoose = require("mongoose");
-const { equal } = require("assert");
-
-let data = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 const app = express();
 app.use(express.static("dist"));
@@ -45,11 +20,22 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/info", (req, res) => {
-  res.send(`
-    Phonebook has info for ${data.length} people <br />
+app.get("/info", async (req, res) => {
+  try {
+    await DBConnect();
+    const people = await Person.find();
+    res.send(`
+    Phonebook has info for ${people.length} people <br />
     ${new Date()}
   `);
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  } finally {
+    mongoose.connection.close();
+  }
 });
 
 app.get("/api/people", async (req, res) => {
